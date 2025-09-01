9cd128c0dcf974ddbe12e1cd20e3b531785da328
root2irrational <newaskershardul@gmail.com>
2025-09-01 10:55:25 +1000

    some random shit

--- exchange.py
+++ exchange.py
@@ -5,20 +5,45 @@ db = conn.cursor()
 
 db.execute('''
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    rank INTEGER,
-    balance REAL
+    userId INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    deposit DECIMAL(18,4),
+    portfolio DECIMAL(18,4),
 )
 ''')
 
-users_data = [
-    ('Black', 1, 10.81),
-    ('Nigger balls', 1, 11.00),
-    ('Inside diddy', 1, 14.00)
-]
+db.execute('''
+CREATE TABLE IF NOT EXISTS Orders (
+    orderId INTEGER PRIMARY KEY,
+    userId INTEGER UNIQUE,
+    instId INTEGER,
+    side ENUM('BUY','SELL'),
+    price DECIMAL(18,4),
+    quantity DECIMAL(18,4),
+    status ENUM('OPEN','FILLED','CANCELLED'),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+)
+''')
 
-db.executemany('INSERT INTO users (name, rank, balance) VALUES (?, ?, ?)', users_data)
+db.execute('''
+CREATE TABLE Trades (
+    tradeId INTEGER PRIMARY KEY,
+    buy_order_id INTEGER,
+    sell_order_id INTEGER,
+    instrument_id INTEGER,
+    price DECIMAL(18,4),
+    quantity DECIMAL(18,4),
+    tradeTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (buy_order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (sell_order_id) REFERENCES Orders(order_id)
+);
+''')
+
+def addUser(userName: str, deposit: float, portfolio: float):
+    user = [userName, deposit, portfolio]
+    db.executemany('INSERT INTO users (userName, deposit, portfolio) VALUES (?, ?, ?)', user)
+    return
 
 conn.commit()
 
